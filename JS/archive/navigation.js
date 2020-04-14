de17e89931ab4648e6f14703b8c1dba5af8fd9b1
ci.macintosh@gmail.com <32034299+cmcntsh@users.noreply.github.com>
2020-04-14 07:21:02 -0600

    base site

---
+++ JS/archive/navigation.js
@@ -0,0 +1,5 @@
+document.write('Navigation');
+document.write('<ul>');
+document.write('    <li><a href="index.html">Home</a></li>');
+document.write('    <li><a href="BaseUnchanged.html" target=_blank>Base</a></li>');
+document.write('</ul>');
